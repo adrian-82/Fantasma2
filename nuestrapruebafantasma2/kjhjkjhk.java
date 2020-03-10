0e6dbdb5135c0469cd2994c33377a9ae6995c672
Adrián <superlaya50@gmail.com>
2020-03-10 13:40:27 +0100

    mierda de mierda

---
+++ nuestrapruebafantasma2/kjhjkjhk.java
@@ -0,0 +1,35 @@
+package nuestrapruebafantasma2;
+
+
+/**
+ * Write a description of class kjhjkjhk here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class kjhjkjhk
+{
+    // instance variables - replace the example below with your own
+    private int x;
+
+    /**
+     * Constructor for objects of class kjhjkjhk
+     */
+    public kjhjkjhk()
+    {
+        // initialise instance variables
+        x = 0;
+    }
+
+    /**
+     * An example of a method - replace this comment with your own
+     * 
+     * @param  y   a sample parameter for a method
+     * @return     the sum of x and y 
+     */
+    public int sampleMethod(int y)
+    {
+        // put your code here
+        return x + y;
+    }
+}
